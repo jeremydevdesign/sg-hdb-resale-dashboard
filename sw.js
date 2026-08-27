485774a3e1ac63257b4a990f32476f20b2817705
jeremydevdesign <293389207+jeremydevdesign@users.noreply.github.com>
2026-08-27 11:43:02 +0700

    Map: rename h1 to 'The island, priced by the block'; sw v57

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // SG HDB Resale Dashboard - service worker
 // Bump CACHE version whenever you change any cached file so clients update.
-const CACHE = 'hdb-resale-v56';
+const CACHE = 'hdb-resale-v57';
 const ASSETS = [
   './',
   './index.html',
